acd8e7231309c014c9580c09cd4ba68f13f71c5c
Serhii Plyska <blysca@newtonideas.com>
2019-12-23 10:47:11 +0200

    completed "Section 5: Mastering Typed Arrays"

---
+++ annotations/arrays.ts
@@ -0,0 +1,27 @@
+const carMakers: string[] = ['ford', 'toyota', 'chevy'];
+const dates = [new Date(), new Date()];
+
+const carsByMake: string[][] = [
+  ['f150'],
+  ['corolla'],
+  ['camaro']
+];
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+// carMakers.push(100);
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
+
+// Flexible types
+const importantDates = [new Date(), '2030-10-10'];
+
+importantDates.push('2030-10-11');
+importantDates.push(new Date());
+// importantDates.push(100);
