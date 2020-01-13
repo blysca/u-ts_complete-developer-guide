b78e9e85ee45af293ac87d966d5ea6a43bd8d275
Serhii Plyska <blysca@newtonideas.com>
2020-01-13 16:44:56 +0200

    DH/TASK261: get "angular" skills

---
+++ stats/src/index.ts
@@ -0,0 +1,20 @@
+import {CsvFileReader} from './CsvFileReader';
+import {MatchResult} from './MatchResult';
+
+const reader = new CsvFileReader('football.csv');
+reader.read();
+
+console.log(reader.data[0][0]);
+
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Mun United won ${manUnitedWins} games`);
