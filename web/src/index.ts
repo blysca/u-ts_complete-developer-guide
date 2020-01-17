3337955ab109312fd8835f41582e1553b08e1468
Serhii Plyska <blysca@newtonideas.com>
2020-01-17 14:35:00 +0200

    completed "Section 13: 148. Adding JSON Server"

---
+++ web/src/index.ts
@@ -0,0 +1,17 @@
+import {User} from './models/User';
+
+const user = new User({name: 'myname', age: 20});
+
+user.on('change', () => {
+  console.log('change #1');
+});
+user.on('change', () => {
+  console.log('change #2');
+});
+user.on('save', () => {
+  console.log('save was triggered');
+  
+});
+
+user.trigger('save');
+user.trigger('oehuh');
---
+++ web/src/models/User.ts
@@ -0,0 +1,39 @@
+interface UserProps {
+  name?: string;
+  age?: number;
+}
+
+type Callback = () => void;
+
+export class User {
+  events: {[key: string]: Callback[]} = {};
+  
+  constructor(private data: UserProps) {
+  }
+  
+  get(propName: string): (number | string) {
+    return this.data[propName];
+  }
+  
+  set(update: UserProps): void {
+    (<any>Object).assign(this.data, update);
+  }
+  
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+  
+  trigger(eventName: string) : void {
+    const handlers = this.events[eventName];
+    
+    if (!handlers || handlers.length === 0) {
+        return;
+    }
+    
+    handlers.forEach( callback => {
+      callback();
+    })
+  }
+}
