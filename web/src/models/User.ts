996742a232e9b0efa9e969259fc31155b9847a81
blysca <blysca@gmail.com>
2020-03-01 17:33:45 +0200

    completed: "Section 13: 177. Final User Refactor"

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,4 +1,7 @@
 import {Model} from "./Model";
+import {Attributes} from "./Attributes";
+import {Eventing} from "./Eventing";
+import {ApiSync} from "./ApiSync";
 
 export interface UserProps {
   id?: number;
@@ -9,5 +12,11 @@ export interface UserProps {
 const rootUrl = 'http://localhost:3000/users';
 
 export class User extends Model<UserProps> {
-
+  static buildUser(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
+    );
+  }
 }
